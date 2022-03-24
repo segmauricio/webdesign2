fec2e3ee7bba65535b3bdbe94507180af498d2ea
Mauricio Segovia <mauricio.segovia12@unae.edu.py>
2022-03-24 20:47:21 -0300

    Primer commit

---
+++ Tarea 240322/js/js.js
@@ -0,0 +1,10 @@
+function TablaMultiplicion(a){
+  console.log("Imprimiendo tabla del "+ a +"");
+	//i++ significa i=i+1;
+
+	for(i = 1;i<=10;i++){
+		console.log(a + "x" + i + "=" + a * i);
+	}
+}
+
+TablaMultiplicion(5);
